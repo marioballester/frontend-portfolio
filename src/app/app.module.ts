7e59178bf902b87d49257b0d61030f387ed5cb8b
Mario <marioballester24@gmail.com>
2023-03-11 02:43:15 -0300

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, ROUTES, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { FotoperfilComponent } from './componentes/fotoperfil/fotoperfil.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { LoginComponent } from './componentes/login/login.component';
+
+
+imports: [
+  BrowserModule,
+  RouterModule.forRoot([
+    {path: "login", component: LoginComponent },
+  ])
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    FotoperfilComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
